629313f3a2f65df8b7bfa282656d7f246b7497ef
sandigst <85585095+sandigst@users.noreply.github.com>
2025-06-14 12:17:52 +0200

    test openlayers example

---
+++ OpenLayersTest/main.js
@@ -0,0 +1,36 @@
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import OSM, {ATTRIBUTION} from 'ol/source/OSM.js';
+
+const openCycleMapLayer = new TileLayer({
+  source: new OSM({
+    attributions: [
+      'All maps © <a href="https://www.opencyclemap.org/">OpenCycleMap</a>',
+      ATTRIBUTION,
+    ],
+    url:
+      'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
+      '?apikey=Your API key from https://www.thunderforest.com/docs/apikeys/ here',
+  }),
+});
+
+const openSeaMapLayer = new TileLayer({
+  source: new OSM({
+    attributions: [
+      'All maps © <a href="https://www.openseamap.org/">OpenSeaMap</a>',
+      ATTRIBUTION,
+    ],
+    url: 'https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png',
+  }),
+});
+
+const map = new Map({
+  layers: [openCycleMapLayer, openSeaMapLayer],
+  target: 'map',
+  view: new View({
+    maxZoom: 18,
+    center: [-244780.24508882355, 5986452.183179816],
+    zoom: 15,
+  }),
+});
